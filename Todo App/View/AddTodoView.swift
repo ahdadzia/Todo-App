bc569961c261bd42309b5965478ae04f18930a0f
ahdadzia <ahda.fakhri@gmail.com>
2021-02-16 11:24:13 +0700

    Hari kedua

--- Todo App/View/AddTodoView.swift
+++ Todo App/View/AddTodoView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct AddTodoView: View {
     
+    @Environment(\.managedObjectContext) var managedObjectContext
     @Environment(\.presentationMode) var presentationMode
     @State private var name: String = ""
     @State private var priority: String = "Normal"
     
     let priorities = ["High", "Normal", "Low"]
+    
+    @State private var errorShowing: Bool = false
+    @State private var errorTitle: String = ""
+    @State private var errorMessage: String = ""
     
     
     var body: some View {
@@ -30,9 +35,27 @@ struct AddTodoView: View {
                     .pickerStyle(SegmentedPickerStyle())
                     
                     Button(action: {
-                        print("Save a new todo item")
+                        if self.name != ""{
+                            let todo = Todo(context: self.managedObjectContext)
+                            todo.name = self.name
+                            todo.priority = self.priority
+                            
+                            do{
+                                try self.managedObjectContext.save()
+                                print("New todo: \(todo.name ?? ""), Priority: \(todo.priority ?? "")")
+                            } catch{
+                                print(error)
+                            }
+                        } else{
+                            self.errorShowing = true
+                            self.errorTitle = "Invalid Name"
+                            self.errorMessage = "Make sure to enter something for\nthe new todo item"
+                            return
+                        }
+                        self.presentationMode.wrappedValue.dismiss()
+                        
                     }){
-                       Text("Save")
+                        Text("Save")
                     }
                 }
                 Spacer()
@@ -44,6 +67,9 @@ struct AddTodoView: View {
                                 }){
                                     Image(systemName: "xmark")
                                 })
+            .alert(isPresented: $errorShowing){
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
         }
     }
 }
--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
     @State private var showingAddTodoView: Bool = false
     
     var body: some View {
@@ -24,7 +26,7 @@ struct ContentView: View {
                                         Image(systemName: "plus")
                                     }
                                     .sheet(isPresented: $showingAddTodoView){
-                                        AddTodoView()
+                                        AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
                                     })
         }
     }
