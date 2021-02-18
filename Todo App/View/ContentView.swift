990381bc322befa5fce573bdaf5808a35af0526e
ahdadzia <ahda.fakhri@gmail.com>
2021-02-18 11:30:15 +0700

    Hari keempat

--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -18,31 +18,37 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-            List{
-                ForEach(self.todos, id: \.self){ todo in
-                    HStack{
-                        Text(todo.name ?? "Unknown")
-                        
-                        Spacer()
-                        
-                        Text(todo.priority ??  "Unknown")
+            ZStack{
+                List{
+                    ForEach(self.todos, id: \.self){ todo in
+                        HStack{
+                            Text(todo.name ?? "Unknown")
+                            
+                            Spacer()
+                            
+                            Text(todo.priority ??  "Unknown")
+                        }
                     }
+                    
+                    .onDelete(perform: deleteTodo)
                 }
+                .navigationBarTitle("Todo", displayMode: .inline)
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing:
+                        Button(action: {
+                            self.showingAddTodoView.toggle()
+                        }){
+                            Image(systemName: "plus")
+                        }
+                        .sheet(isPresented: $showingAddTodoView){
+                            AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+                        })
                 
-                .onDelete(perform: deleteTodo)
+                if todos.count == 0{
+                    EmptyListView()
+                }
             }
-            .navigationBarTitle("Todo", displayMode: .inline)
-            .navigationBarItems(
-                leading: EditButton(),
-                trailing:
-                    Button(action: {
-                        self.showingAddTodoView.toggle()
-                    }){
-                        Image(systemName: "plus")
-                    }
-                    .sheet(isPresented: $showingAddTodoView){
-                        AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
-                    })
         }
     }
     
